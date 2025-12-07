36dfe794d88bf170a0cd2a9b7ae79108df26a8cf
Patrick Jedrzejko <patrickjedrzejko@gmail.com>
2025-12-07 15:31:13 -0500

    ternary operator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,11 @@
+public class TernaryOperator   {
+    public static void main(String[] args) {
+
+        int number = 4;
+
+        String evenOrOdd = number % 2 == 0 ? "EVEN" : "ODD";
+
+        System.out.println(evenOrOdd);
+
+    }
+}
